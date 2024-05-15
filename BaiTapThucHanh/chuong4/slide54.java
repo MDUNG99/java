66bfd7a3ef0a23dc99a711d0495f588cb3935460
MDUNG99 <dungmanhtran203@gmai.com>
2024-05-15 16:42:59 +0700

    chuong4

---
+++ BaiTapThucHanh/chuong4/slide54.java
@@ -0,0 +1,35 @@
+package chuong4;
+
+import java.util.HashSet;
+
+import.java.until.hashset;
+import.java.until.Scanner
+
+public class slide54 {
+    public static void main (sting [] args){
+        int number ;
+        HashSet<Integer> HashsetInteger = new Hashset <>();
+        Scanner sc = new Scanner(sytem.in);
+        HashsetInteger.add(0);
+        HashsetInteger.add(3);
+        HashsetInteger.add(1);
+        HashsetInteger.add(4);
+        HashetInteger.add(2);
+        HashetInteger.add(8);
+        sytem.uot.println ("cac phan tu trong HashsetInteger: ");
+        sytem.uot.println (HashsetInteger);
+        sytem.out.prinln ("nhap phan tu can them: ");
+        number = Scanner.nextInt();
+        if(!HashsetInter.contains(number));{
+            HashsetInter.add(number);
+            sytem.uot.println ("them thanh cong: ");
+            sytem.out.println ("cac phan tu trong HashsetInter sau khi them: ");
+            sytem.uot.println (HashserInter );}
+        els{
+            sytem.out.println ("phan tu" + number + "da ton tai !");
+    }
+        } 
+}
+
+    
+
